f2c460fff40896382679a1fdd9c9c53e47dcf5fe
Camila Lucía Pérez Liria <camiluc.peli@hotmail.com>
2019-06-22 16:44:26 +0200

    Basic entity for events

---
+++ MotoGPCalendar.Domain/Entities/MotoGPEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotoGPCalendar.Domain.Entities
+{
+    class MotoGPEvent
+    {
+        public DateTime EventDate { get; set; }
+        public string EventName { get; set; }
+        public string CircuitName { get; set; }
+        public string CountryName { get; set; }
+        public double CircuitLength { get; set; }
+        public  int CircuitCurves { get; set; }
+        public double CircuitWidth { get; set; }
+        public double CircuitLongerStraight { get; set; }
+        public double CircuitRecord { get; set; }
+    }
+}
