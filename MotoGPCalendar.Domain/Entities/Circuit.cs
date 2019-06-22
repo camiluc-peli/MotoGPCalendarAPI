2d9e801db398d8912897f4cc7721c8beea3a098e
Camila Lucía Pérez Liria <camiluc.peli@hotmail.com>
2019-06-22 16:51:37 +0200

    New entity for Circuits

---
+++ MotoGPCalendar.Domain/Entities/Circuit.cs
@@ -0,0 +1,14 @@
+﻿namespace MotoGPCalendar.Domain.Entities
+{
+    class Circuit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public double Length { get; set; }
+        public int CurvesNumber { get; set; }
+        public double Width { get; set; }
+        public double LongerStraight { get; set; }
+        public double Record { get; set; }
+    }
+}
--- MotoGPCalendar.Domain/Entities/MotoGPEvent.cs
+++ MotoGPCalendar.Domain/Entities/MotoGPEvent.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MotoGPCalendar.Domain.Entities
 {
     class MotoGPEvent
     {
+        public int Id { get; set; }
         public DateTime EventDate { get; set; }
         public string EventName { get; set; }
-        public string CircuitName { get; set; }
-        public string CountryName { get; set; }
-        public double CircuitLength { get; set; }
-        public  int CircuitCurves { get; set; }
-        public double CircuitWidth { get; set; }
-        public double CircuitLongerStraight { get; set; }
-        public double CircuitRecord { get; set; }
+        public Circuit Circuit { get; set; }
     }
 }
