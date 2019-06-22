4981d10cc3916766bb4443f41c4b2ba0b2445bf4
Camila Lucía Pérez Liria <camiluc.peli@hotmail.com>
2019-06-22 17:02:01 +0200

    Entity for countries added

--- MotoGPCalendar.Domain/Entities/Circuit.cs
+++ MotoGPCalendar.Domain/Entities/Circuit.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string CountryName { get; set; }
+        public Country Country { get; set; }
         public double Length { get; set; }
-        public int CurvesNumber { get; set; }
         public double Width { get; set; }
+        public int CurvesNumber { get; set; }
         public double LongerStraight { get; set; }
         public double Record { get; set; }
     }
---
+++ MotoGPCalendar.Domain/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotoGPCalendar.Domain.Entities
+{
+    class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Code { get; set; }
+        public string Alpha2Code { get; set; }
+        public string Alpha3Code { get; set; }
+    }
+}
