3c372f0c49a6dfe9a725b1bea7ff12b185e8e9c3
Camila Lucía Pérez Liria <camiluc.peli@hotmail.com>
2019-06-23 04:11:26 +0200

    Controllers and business layer created (not implemented)

---
+++ MotoGPCalendar.Business/Handlers/CountryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MotoGPCalendar.Domain.Entities;
+using System.Collections.Generic;
+
+namespace MotoGPCalendar.Business.Handlers
+{
+    public class CountryHandler
+    {
+        public List<Country> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+        public Country GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Insert(Country country)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MotoGPCalendar.Business/Handlers/MotoGPEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MotoGPCalendar.Domain.Entities;
+using System.Collections.Generic;
+
+namespace MotoGPCalendar.Business.Handlers
+{
+    public class MotoGPEventHandler
+    {
+        public List<MotoGPEvent> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+        public MotoGPEvent GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Insert(MotoGPEvent motoGPevent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
