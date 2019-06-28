1c873b114c1a9c50dc5a7ac3700f641e1d99801d
Camila <camiluc.peli@gmail.com>
2019-06-28 11:16:46 +0200

    Adding Id to DTOs to work when front end calls.

--- MotoGPCalendar.DTOs/EventDTO.cs
+++ MotoGPCalendar.DTOs/EventDTO.cs
@@ -4,6 +4,7 @@ namespace MotoGPCalendar.DTOs
 {
     public class EventDTO
     {
+        public int Id { get; set; }
         public DateTime EventDate { get; set; }
         public string EventName { get; set; }
         public string CircuitName { get; set; }
